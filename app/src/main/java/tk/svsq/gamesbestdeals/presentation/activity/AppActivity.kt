ce78fc56a36e4f9e2f57ca776171061c2297898d
Slava Bilokur <vbilokur@danavero.com>
2021-09-28 19:45:41 +0300

    added bottom menu

--- app/src/main/java/tk/svsq/gamesbestdeals/presentation/activity/AppActivity.kt
+++ app/src/main/java/tk/svsq/gamesbestdeals/presentation/activity/AppActivity.kt
@@ -2,18 +2,27 @@ package tk.svsq.gamesbestdeals.presentation.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import tk.svsq.gamesbestdeals.R
 import tk.svsq.gamesbestdeals.databinding.ActivityAppBinding
-import tk.svsq.gamesbestdeals.presentation.common.viewBinding
 
 @AndroidEntryPoint
 class AppActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAppBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityAppBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
